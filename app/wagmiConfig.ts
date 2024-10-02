cb4672fc84372564a7ec7c25d84d992879f6d7fd
desamtralized <sambarbosaa@gmail.com>
2024-10-02 13:43:59 -0500

    chore: remove comments

--- app/wagmiConfig.ts
+++ app/wagmiConfig.ts
@@ -3,7 +3,7 @@ import { mainnet } from "wagmi/chains";
 
 export default getDefaultConfig({
   appName: 'Yieldi',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECTID || '', // TEMP ID, add new one in ENV
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECTID || '',
   chains: [mainnet],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+  ssr: true,
 });
