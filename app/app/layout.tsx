3d1dbf93058e2f14b99627850ca8783d5aec013f
desamtralized <sambarbosaa@gmail.com>
2024-10-14 12:51:20 +0000

    chore: add twMerge to app repo, fix horizontal margins

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
             ]}
             buttons={[{ component: <WalletButton /> }]}
           />
-          <div className="max-w-5xl mx-auto p-4">{children}</div>
+          <div className="mx-20 py-4">{children}</div>
         </Providers>
       </body>
     </html>
