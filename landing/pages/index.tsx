e0e3348bbc6f5397f69f33d2705050ed6b72cd0e
Matthew Nguyen <mnguyen916@gmail.com>
2024-10-21 22:42:51 -0700

    feat(landing): add metatags to landing page
    We will most likely need the same metatags for the app but we can
    refactor later.

--- landing/pages/index.tsx
+++ landing/pages/index.tsx
@@ -6,16 +6,58 @@ import {
   HowDoesItWork,
   KeyComponents,
 } from "@/components/common";
+import Head from "next/head";
 
 const Home = () => (
-  <div className="overflow-x-hidden">
-    <Hero />
-    <Info />
-    <HowDoesItWork />
-    <Solution />
-    <KeyComponents />
-    <CTA />
-  </div>
+  <>
+    <Head>
+      <title>Yieldi</title>
+
+      <meta property="url" content="https://www.yieldi.xyz" />
+      <meta
+        property="description"
+        content="The only DeFi venue to stake and earn native Bitcoin yield with 15+ supported L1 assets."
+      />
+
+      {/* Open Graph metatags */}
+      <meta
+        property="og:title"
+        content="Yieldi - The Yield Layer for Cross-Chain"
+      />
+      <meta
+        property="og:description"
+        content="The only DeFi venue to stake and earn native Bitcoin yield with 15+ supported L1 assets."
+      />
+      <meta property="og:image" content="/images/og-image.png" />
+      <meta property="og:url" content="https://www.yieldi.xyz" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter metatags */}
+      <meta
+        name="twitter:title"
+        content="Yieldi - The Yield Layer for Cross-Chain"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:description"
+        content="The only DeFi venue to stake and earn native Bitcoin yield with 15+ supported L1 assets."
+      />
+
+      {/* Misc */}
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="canonical" />
+    </Head>
+    <div className="overflow-x-hidden">
+      <Hero />
+      <Info />
+      <HowDoesItWork />
+      <Solution />
+      <KeyComponents />
+      <CTA />
+    </div>
+  </>
 );
 
 export default Home;
