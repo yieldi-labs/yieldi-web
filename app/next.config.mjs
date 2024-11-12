c151445900d79f227081489164b38b74ed343b4d
desamtralized <sambarbosaa@gmail.com>
2024-11-11 19:17:30 -0500

    revert changes to next.config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,8 +1,5 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,48 +17,7 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  webpack: (config, { isServer, webpack }) => {
-    if (!isServer) {
-      config.plugins.push(new NodePolyfillPlugin());
-
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          global: [require.resolve("global"), "default"],
-          process: "process/browser",
-        }),
-      );
-
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          "global.crypto": "crypto",
-          "global.msCrypto": "crypto",
-          "global.process": "process",
-          "global.Uint8Array": JSON.stringify(Uint8Array),
-        }),
-      );
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        path: require.resolve("path-browserify"),
-        process: require.resolve("process/browser"),
-        stream: require.resolve("stream-browserify"),
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        os: require.resolve("os-browserify/browser"),
-      };
-    }
-
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      syncWebAssembly: true,
-      topLevelAwait: true,
-    };
-
+  webpack: (config) => {
     // Ensure shared folder is transpiled
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
