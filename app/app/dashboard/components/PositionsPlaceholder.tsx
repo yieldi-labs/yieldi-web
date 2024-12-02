048f9f0d1fe7c16402b4c96f2b5318be6bb44dce
hippocampus-web3 <hippocampus.web3@proton.me>
2024-12-02 17:49:28 +0100

    Update placeholder copy

--- app/app/dashboard/components/PositionsPlaceholder.tsx
+++ app/app/dashboard/components/PositionsPlaceholder.tsx
@@ -1,12 +1,14 @@
 import TranslucentCard from "@/app/TranslucentCard";
+import { useAppState } from "@/utils/context";
 import React from "react";
 
 export default function PositionsPlaceholder() {
+  const { wallet } = useAppState();
   return (
     <TranslucentCard>
       <div className="w-full h-24 p-2.5 border-4 border-white justify-center items-center flex">
         <p className="text-neutral-800 font-medium text-base">
-          Your liquidity positions will appear here.
+          { wallet ? "Your liquidity positions will appear here." : "Connect your wallet to view your liquidity positions" }
         </p>
       </div>
     </TranslucentCard>
