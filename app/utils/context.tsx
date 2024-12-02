1b9403f06b396668beb72d0c0c800370526209d9
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-12-02 05:29:55 -0800

    added useWalletTokens hook

--- app/utils/context.tsx
+++ app/utils/context.tsx
@@ -15,7 +15,6 @@ import {
 } from "./wallet/constants";
 import { GetConnectorsReturnType } from "wagmi/actions";
 import { connectEVMWallet, connectUTXOWallet } from "./wallet/walletConnect";
-import { TokenData, WalletTokensData } from "./interfaces";
 
 interface AppStateContextType {
   isWalletModalOpen: boolean;
@@ -26,8 +25,6 @@ interface AppStateContextType {
   getChainKeyFromChain: (chain: string) => ChainKey;
   toggleWalletDrawer: () => void;
   isWalletDrawerOpen: boolean;
-  walletTokensData: WalletTokensData;
-  setWalletTokensData: React.Dispatch<React.SetStateAction<WalletTokensData>>;
 }
 
 const AppStateContext = createContext<AppStateContextType | undefined>(
@@ -38,36 +35,6 @@ export const AppStateProvider = ({ children }: { children: ReactNode }) => {
   const [isWalletModalOpen, setIsWalletModalOpen] = useState(false);
   const [isWalletDrawerOpen, setIsWalletDrawerOpen] = useState(false);
   const [walletsState, setWalletsState] = useState<ConnectedWalletsState>({});
-  const initialWalletTokensData: WalletTokensData = {
-    [ChainKey.ARBITRUM]: {},
-    [ChainKey.AVALANCHE]: {},
-    [ChainKey.BASE]: {},
-    [ChainKey.BITCOIN]: {},
-    [ChainKey.BITCOINCASH]: {},
-    [ChainKey.BLAST]: {},
-    [ChainKey.BSCCHAIN]: {},
-    [ChainKey.CRONOSCHAIN]: {},
-    [ChainKey.DASH]: {},
-    [ChainKey.DOGECOIN]: {},
-    [ChainKey.DYDX]: {},
-    [ChainKey.ETHEREUM]: {},
-    [ChainKey.GAIACHAIN]: {},
-    [ChainKey.KUJIRA]: {},
-    [ChainKey.LITECOIN]: {},
-    [ChainKey.MAYACHAIN]: {},
-    [ChainKey.OPTIMISM]: {},
-    [ChainKey.POLKADOT]: {},
-    [ChainKey.POLYGON]: {},
-    [ChainKey.SOLANA]: {},
-    [ChainKey.SUI]: {},
-    [ChainKey.THORCHAIN]: {},
-    [ChainKey.TON]: {},
-    [ChainKey.ZKSYNC]: {},
-  };
-  const [walletTokensData, setWalletTokensData] = useState<WalletTokensData>(
-    initialWalletTokensData
-  );
-
   const toggleWalletModal = () => {
     setIsWalletModalOpen((prevState) => !prevState);
   };
@@ -87,6 +54,9 @@ export const AppStateProvider = ({ children }: { children: ReactNode }) => {
       }
       case "DOGE": {
         return ProviderKey.DOGECOIN;
+      }
+      case "THOR": {
+        return ProviderKey.THORCHAIN;
       }
       default: {
         return ProviderKey.EVM;
@@ -297,8 +267,6 @@ export const AppStateProvider = ({ children }: { children: ReactNode }) => {
         getChainKeyFromChain,
         isWalletDrawerOpen,
         toggleWalletDrawer,
-        walletTokensData,
-        setWalletTokensData,
       }}
     >
       {children}
