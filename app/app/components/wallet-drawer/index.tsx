3a3844a9031ea5f903cad9a82eee82136e9dd653
hippocampus-web3 <hippocampus.web3@proton.me>
2024-12-09 19:50:16 +0100

    Remove unnecesary useEffect

--- app/app/components/wallet-drawer/index.tsx
+++ app/app/components/wallet-drawer/index.tsx
@@ -35,7 +35,22 @@ const Component: FC = () => {
     refreshBalances();
   };
 
+<<<<<<< HEAD
   const { copy } = useCopyToClipboard()
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+  const { copy } = useCopyToClipboard()
+=======
+  useEffect(() => {
+    refreshBalances();
+  }, [refreshBalances]);
+
+=======
+>>>>>>> c5420b3 (Remove unnecesary useEffect)
+  const { copy } = useCopyToClipboard();
+>>>>>>> 87053da (Apply formater)
+>>>>>>> f04c479 (Remove unnecesary useEffect)
 
   return (
     isWalletDrawerOpen && (
