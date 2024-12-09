f97dcb0b70e960baca0b04fea60c293cf0d9ad09
hippocampus-web3 <hippocampus.web3@proton.me>
2024-12-09 13:28:11 +0100

    Remove problematic dependency with vercel default config

--- app/hooks/useUTXO.ts
+++ app/hooks/useUTXO.ts
@@ -4,7 +4,6 @@ import {
   defaultBTCParams,
 } from "@xchainjs/xchain-bitcoin";
 import { Client as DogeClient, defaultDogeParams } from "@xchainjs/xchain-doge";
-import { Client as UxtoClient } from "@xchainjs/xchain-utxo";
 import {
   defaultLtcParams,
   Client as LitecoinClient,
