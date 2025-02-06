ca6b2aa7f9174a0166224f808181fd95cab75175
hippocampus-web3 <hippocampus.web3@proton.me>
2025-02-06 19:50:04 +0100

    Enable sentry

---
+++ app/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://53d47f2c816b64056b87e52f96b1eb1a@o4508773676679168.ingest.us.sentry.io/4508773679628288",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
