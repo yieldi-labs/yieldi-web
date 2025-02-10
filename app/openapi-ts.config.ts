3cdb72d6ac5ab337cf81e0d19a831b5c99be23a6
hippocampus-web3 <hippocampus.web3@proton.me>
2025-02-10 18:22:27 +0100

    create commands for generate stagenet clients

--- app/openapi-ts.config.ts
+++ app/openapi-ts.config.ts
@@ -20,6 +20,26 @@ const thornodeAPI = defineConfig({
   plugins: ['@hey-api/client-fetch'],
 });
 
+const midgardStagenetAPI = defineConfig({
+  input: "https://stagenet-midgard.ninerealms.com/v2/swagger.json",
+  output: {
+    format: "prettier",
+    lint: "eslint",
+    path: "midgard",
+  },
+  plugins: ['@hey-api/client-fetch'],
+});
+
+const thornodeStagenetAPI = defineConfig({
+  input: "https://stagenet-thornode.ninerealms.com/thorchain/doc/openapi.yaml",
+  output: {
+    format: "prettier",
+    lint: "eslint",
+    path: "thornode",
+  },
+  plugins: ['@hey-api/client-fetch'],
+});
+
 let configExport;
 switch (process.env.API_SPEC) {
   case "midgard": {
@@ -28,6 +48,14 @@ switch (process.env.API_SPEC) {
   }
   case "thornode": {
     configExport = thornodeAPI;
+    break;
+  }
+  case "midgard-stagenet": {
+    configExport = midgardStagenetAPI;
+    break;
+  }
+  case "thornode-stagenet": {
+    configExport = thornodeStagenetAPI;
     break;
   }
   default:
